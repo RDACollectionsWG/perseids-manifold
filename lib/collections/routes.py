dfc8f9cb47af609b5d0e7e6d686f33a986fc973b
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-01-26 08:05:17 -0500

    Updated collections routes

--- lib/collections/routes.py
+++ lib/collections/routes.py
@@ -5,7 +5,7 @@ Sets the mapping between `url_endpoints` and `view functions`.
 """
 
 routes = [
-    ("/collections/<id>", {'view_func': collections['index'], 'methods': ["GET", "POST", "PUT", "DELETE"]}),
+    ("/collections/<path:id>", {'view_func': collections['index'], 'methods': ["GET", "POST", "PUT", "DELETE"]}),
     ("/collections", {'view_func': collections['index'], 'methods': ["GET", "POST"], 'defaults':{'id': None}}),
     ("/collections/<id>/capabilities", {'view_func': collections['capabilities'], 'methods': ["GET"]}),
     ("/collections/<id>/ops/findMatch", {'view_func': collections['findmatch'], 'methods': ["POST"]}),
