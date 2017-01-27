19c12b784c2c46dcc868d52629b2ae84265eaa9f
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-01-27 08:10:15 -0500

    Error handling

--- run.py
+++ run.py
@@ -3,9 +3,12 @@ from flask import Flask
 from lib.service.routes import routes as r_service
 from lib.collections.routes import routes as r_collections
 from lib.members.routes import routes as r_members
+from lib.utils.json_encoder import MyJSONEncoder
+from lib.collections.errors import activate
 
 app = Flask(__name__)
-
+app.json_encoder = MyJSONEncoder
+activate(app)
 
 for (url, kwargs) in r_service + r_collections + r_members:
     app.add_url_rule(url, **kwargs)
