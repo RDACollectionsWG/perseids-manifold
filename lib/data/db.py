cd7c4adbd3155e62d82dae060ab5be7b11efb7c9
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-02-17 15:19:05 -0500

    Draft DB interface

--- lib/data/db.py
+++ lib/data/db.py
@@ -1,6 +1,44 @@
 from ..collections.models import *
 from ..members.models import *
 from ..service.models import *
+from json import load
+from os import listdir
+from os.path import abspath, isdir, join
+
+
+class DataBase:
+
+    def __init__(self, d_data='data', d_collection='_collection.json'):
+        self.d_data = d_data
+        self.d_collection = d_collection
+
+    def __load_json__(self, filename):
+        with open(filename) as filecontent:
+            return load(filecontent)
+
+    def getCollections(self, cid):
+        if not cid:
+            return self.getCollections([d for d in listdir(self.d_data) if isdir(join(self.d_data, d))])
+        else:
+            if not type(cid) in (type([]), type(()), type(set())):
+                cid = [cid]
+            return [self.__load_json__(join(self.d_data, id, self.d_collection)) for id in cid]
+
+    def getMembers(self, cid, mid):
+        assert cid
+        if not mid:
+            return self.getMembers(cid, [m for m in listdir(join(self.d_data, cid)) if m.endswith('.json') and m is not self.d_collection])
+        else:
+            if not type(mid) in (type([]), type(()), type(set())):
+                mid = [mid]
+            return [self.__load_json__(join(self.d_data, cid, id)) for id in mid]
+
+    def getService(self):
+        return ''
+
+
+
+
 
 cites = {
     'urn:cite:test_collections.1': CollectionObject(
@@ -26,3 +64,5 @@ members = {
 }
 
 service = Service(supported_collections_operations=[Operations.findMatch, Operations.flatten])
+
+dbpath = abspath('./')
