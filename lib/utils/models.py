c9e40223da78ae570382e7079eee97b7c814652c
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-02-17 15:15:28 -0500

    Add Model.apply for deserialization

--- lib/utils/models.py
+++ lib/utils/models.py
@@ -1,3 +1,11 @@
 class Model(object):
+
+    @classmethod
+    def apply(cls, dct):
+        try:
+            return cls(**dct)
+        except:
+            return None
+
     def dict(self):
         return {key: value for key, value in self.__dict__.items() if not key.startswith("__")}
