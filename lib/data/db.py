6f0f448d3b6d13038d1f31f4db27403e802f2392
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-02-21 16:48:57 -0500

    DB methods for GET routes

--- lib/data/db.py
+++ lib/data/db.py
@@ -8,61 +8,34 @@ from os.path import abspath, isdir, join
 
 class DataBase:
 
-    def __init__(self, d_data='data', d_collection='_collection.json'):
+    def __init__(self, d_data='data', d_collection='_collection.json', d_service='_service.json'):
         self.d_data = d_data
         self.d_collection = d_collection
+        self.d_service = d_service
 
     def __load_json__(self, filename):
         with open(filename) as filecontent:
             return load(filecontent)
 
-    def getCollections(self, cid):
+    def getCollections(self, cid=None):
         if not cid:
             return self.getCollections([d for d in listdir(self.d_data) if isdir(join(self.d_data, d))])
         else:
             if not type(cid) in (type([]), type(()), type(set())):
                 cid = [cid]
-            return [self.__load_json__(join(self.d_data, id, self.d_collection)) for id in cid]
+            return [CollectionObject(**self.__load_json__(join(self.d_data, id.replace('/', '∕'), self.d_collection))) for id in cid]
 
-    def getMembers(self, cid, mid):
+    def getMembers(self, cid, mid=None):
         assert cid
         if not mid:
-            return self.getMembers(cid, [m for m in listdir(join(self.d_data, cid)) if m.endswith('.json') and m is not self.d_collection])
+            return self.getMembers(cid, [m[:-5] for m in listdir(join(self.d_data, cid.replace('/', '∕'))) if (m.endswith('.json') and m != self.d_collection)])
         else:
             if not type(mid) in (type([]), type(()), type(set())):
                 mid = [mid]
-            return [self.__load_json__(join(self.d_data, cid, id)) for id in mid]
+            return [MemberItem(**self.__load_json__(join(self.d_data, cid.replace('/', '∕'), id.replace('/', '∕')+'.json'))) for id in mid]
 
     def getService(self):
-        return ''
+        return Service(self.__load_json__(join(self.d_data, self.d_service)))
 
 
-
-
-
-cites = {
-    'urn:cite:test_collections.1': CollectionObject(
-        "urn:cite:test_collections.1",
-        CollectionCapabilities(True, False, False, True, True, "", 5),
-        CollectionProperties("perseids:me", "CCbySA", "https://github.com/perseids-project/CITE-JSON-LD/blob/master/templates/img/SCHEMA.md", False, [], "https://github.com/perseids-project/CITE-JSON-LD/blob/master/templates/img/SCHEMA.md"),
-        ""
-    ),
-    'urn:cite:test_collections.2': CollectionObject(
-        "urn:cite:test_collections.2",
-        CollectionCapabilities(True, False, False, True, True, "", 5),
-        CollectionProperties("perseids:me", "CCbySA", "https://github.com/perseids-project/CITE-JSON-LD/blob/master/templates/img/SCHEMA.md", False, [], "https://github.com/perseids-project/CITE-JSON-LD/blob/master/templates/img/SCHEMA.md"),
-        ""
-    )
-}
-
-members = {
-    "http://example.org/mem/1": MemberItem("http://example.org/mem/1","http://example.org/loc/1"),
-    "http://example.org/mem/2": MemberItem("http://example.org/mem/2","http://example.org/loc/2"),
-    "http://example.org/mem/3": MemberItem("http://example.org/mem/3","http://example.org/loc/3"),
-    "http://example.org/mem/4": MemberItem("http://example.org/mem/4","http://example.org/loc/4"),
-    "http://example.org/mem/5": MemberItem("http://example.org/mem/5","http://example.org/loc/5")
-}
-
-service = Service(supported_collections_operations=[Operations.findMatch, Operations.flatten])
-
-dbpath = abspath('./')
+db = DataBase()
