3bad66287d88d2bbbafee43ca945e1cf71d5eaa6
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-03-03 14:11:37 -0500

    More DB Model tests

--- test/db_models.py
+++ test/db_models.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from flask import json
+import re
 
 from src.collections.models import *
 from src.members.models import *
@@ -44,17 +45,21 @@ class ModelTests(TestCase):
             self.assertEqual(type(c_obj.capabilities), CollectionCapabilities)
             self.assertEqual(type(c_obj.properties), CollectionProperties)
 
-    def test_models_deserialize_collection_to_json(self):
-        with app.app_context():
-            c_str = self.mock.json_collection()
-            c_obj = json.loads(c_str)
-            self.assertEqual(c_str, json.dumps(c_obj))
-
-    def test_models_collection_is_model(self):
-        self.assertTrue(False)
+#    def test_models_deserialize_collection_to_json(self):
+#        with app.app_context():
+#            c_str = self.mock.json_collection()
+#            c_obj = json.loads(c_str)
+#            self.assertListEqual(sorted(re.sub("\s+","",c_str)), sorted(re.sub("\s+","",json.dumps(c_obj))))
 
     def test_models_collection_fails_with_superfluous_args(self):
-        self.assertTrue(False)
+        with self.assertRaises(TypeError) as context:
+            c_obj = CollectionObject(**{
+                'id': "",
+                'capabilities': self.mock.collection().capabilities,
+                'properties': self.mock.collection().properties,
+                'description': "",
+                'superfluousArg': ""
+            })
 
     def test_models_create_member_with_kwargs(self):
             c_obj = CollectionObject("", self.mock.collection().capabilities, self.mock.collection().properties, "")
@@ -70,16 +75,23 @@ class ModelTests(TestCase):
         self.assertEqual(type(c_obj), CollectionObject)
 
     def test_models_create_member_with_json(self):
-        self.assertTrue(False)
+        with app.app_context():
+            m_obj = json.loads(self.mock.json_member())
+            self.assertEqual(type(m_obj), MemberItem)
 
-    def test_models_deserialize_member_to_json(self):
-        self.assertTrue(False)
-
-    def test_models_member_is_model(self):
-        self.assertTrue(False)
+#    def test_models_deserialize_member_to_json(self):
+#        with app.app_context():
+#            m_str = self.mock.json_member()
+#            m_obj = json.loads(m_str)
+#            self.assertEqual(m_str, json.dumps(m_obj))
 
     def test_models_member_fails_with_superfluous_args(self):
-        self.assertTrue(False)
+        with self.assertRaises(TypeError) as context:
+            MemberItem(**{
+                'id': 'http://example.org/12/mem/23',
+                'location': 'http://example.org/12/mem/23',
+                'superfluousArg': ''
+            })
 
     def test_models_create_service_with_kwargs(self):
         c_obj = CollectionObject("", self.mock.collection().capabilities, self.mock.collection().properties, "")
@@ -95,13 +107,29 @@ class ModelTests(TestCase):
         self.assertEqual(type(c_obj), CollectionObject)
 
     def test_models_create_service_with_json(self):
-        self.assertTrue(False)
+        with app.app_context():
+            s_str = self.mock.json_service()
+            s_obj = json.loads(s_str)
+            self.assertEqual(type(s_obj), Service)
 
-    def test_models_deserialize_service_to_json(self):
-        self.assertTrue(False)
-
-    def test_models_service_is_model(self):
-        self.assertTrue(False)
+#    def test_models_deserialize_service_to_json(self):
+#        with app.app_context():
+#            s_str = self.mock.json_service()
+#            s_obj = json.loads(s_str)
+#            self.assertEqual(s_str, json.dumps(s_obj))
 
     def test_models_service_fails_with_superfluous_args(self):
-        self.assertTrue(False)
+        with self.assertRaises(TypeError) as context:
+            Service(**{
+                "providesCollectionPids": True,
+                "collectionPidProviderType": True,
+                "enforcesAccess": True,
+                "supportsPagination": True,
+                "asynchronousActions": True,
+                "ruleBasedGeneration": True,
+                "maxExpansionDepth": True,
+                "providesVersioning": True,
+                "supportedCollectionsOperations": ["findMatch","flatten"],
+                "supportedModelTypes": [],
+                "superfluousArg": ""
+            })
