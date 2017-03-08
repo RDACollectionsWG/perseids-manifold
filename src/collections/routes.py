7d9b39eac825f91604eda171fdf614e8673078c8
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-03-08 17:28:39 -0500

    Bug fix

--- src/collections/routes.py
+++ src/collections/routes.py
@@ -5,8 +5,9 @@ Sets the mapping between `url_endpoints` and `view functions`.
 """
 
 routes = [
-    ("/collections/<path:id>", {'view_func': collections['index'], 'methods': ["GET", "POST", "PUT", "DELETE"]}),
-    ("/collections/", {'view_func': collections['index'], 'methods': ["GET", "POST"], 'defaults':{'id': None}}),
+    ("/collections", {'view_func': collections['index'], 'methods': ["GET", "POST"], 'defaults':{'id': None}}),
+    ("/collections/", {'view_func': collections['redirect'], 'methods': ["GET", "POST"], 'defaults':{'id': None}}),
+    ("/collections/<path:id>", {'view_func': collections['index'], 'methods': ["GET", "PUT", "DELETE"]}), #, 'defaults':{'id': None}}),
     ("/collections/<path:id>/capabilities", {'view_func': collections['capabilities'], 'methods': ["GET"]}),
     ("/collections/<path:id>/ops/findMatch", {'view_func': collections['findmatch'], 'methods': ["POST"]}),
     ("/collections/<path:id>/ops/intersection/<path:other_id>", {'view_func': collections['intersection'], 'methods': ["GET"]}),
