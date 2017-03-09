949c0d174e15d8252edfd943ae3cc543e6357f01
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-03-09 14:58:53 -0500

    Remove defaults from Member routes

--- src/members/routes.py
+++ src/members/routes.py
@@ -5,7 +5,8 @@ Sets the mapping between `url_endpoints` and `view functions`.
 """
 
 routes = [
+    ("/collections/<path:id>/members", {'view_func': members['members'], 'methods': ["GET", "POST"]}),
+    ("/collections/<path:id>/members/", {'view_func': members['members'], 'methods': ["GET", "POST"]}),
     ("/collections/<path:id>/members/<path:mid>", {'view_func': members['members'], 'methods': ["GET", "PUT", "DELETE"]}),
-    ("/collections/<path:id>/members/", {'view_func': members['members'], 'methods': ["GET", "POST"], 'defaults':{'mid': None}}),
     ("/collections/<path:id>/members/<path:mid>/properties/<path:property>", {'view_func': members['properties'], 'methods': ["GET", "PUT", "DELETE"]})
 ]
