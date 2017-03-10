8df63d3a53e3a6e953cd5c95d16519d5985181e0
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-03-10 16:38:10 -0500

    Add DB interface fix #6

--- src/data/db.py
+++ src/data/db.py
@@ -1,76 +1,40 @@
-from flask.json import load, dump
-import os
-import random
-import string
-from os import listdir, remove
-from os.path import abspath, isdir, join
-from shutil import rmtree
+import abc
 
 
-class DataBase:
+class DBInterface(object, metaclass=abc.ABCMeta):
 
-    def __init__(self, d_data='data', d_collection='_collection.json', d_service='_service.json'):
-        self.d_data = d_data
-        self.d_collection = d_collection
-        self.d_service = d_service
+    @abc.abstractmethod
+    def get_collection(self, id:None):
+        pass
 
-    def __load_json__(self, filename):
-        with open(filename) as filecontent:
-            return load(filecontent)
+    @abc.abstractmethod
+    def set_collection(self, c_obj):
+        pass
 
-    def __write_json__(self, filename, object):
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
-        with open(filename,"w") as file:
-            dump(object, file)
+    @abc.abstractmethod
+    def del_collection(self, id):
+        pass
 
-    def getCollections(self, cid=None):
-        if cid is None:
-            ids = [d for d in listdir(self.d_data) if isdir(join(self.d_data, d))]
-            return self.getCollections(ids)
-        else:
-            if not type(cid) in (type([]), type(()), type(set())):
-                cid = [cid]
-            return [self.__load_json__(join(self.d_data, id.replace('/', '∕'), self.d_collection)) for id in cid]
+    @abc.abstractmethod
+    def get_member(self, id:None):
+        pass
 
-    def getMembers(self, cid, mid=None):
-        assert cid
-        if mid is None:
-            return self.getMembers(cid, [m[:-5] for m in listdir(join(self.d_data, cid.replace('/', '∕'))) if (m.endswith('.json') and m != self.d_collection)])
-        else:
-            if not type(mid) in (type([]), type(()), type(set())):
-                mid = [mid]
-            return [self.__load_json__(join(self.d_data, cid.replace('/', '∕'), id.replace('/', '∕')+'.json')) for id in mid]
+    @abc.abstractmethod
+    def set_member(self, m_obj):
+        pass
 
-    def getService(self):
-        return self.__load_json__(join(self.d_data, self.d_service))
+    @abc.abstractmethod
+    def del_member(self, id):
+        pass
 
-    def setService(self, sObject):
-        self.__write_json__(join(self.d_data, self.d_service), sObject)
+    @abc.abstractmethod
+    def get_service(self):
+        pass
 
-    '''
-        @:returns CollectionObject or Error
-    '''
-    def setCollection(self,cObject):
-        filename = join(self.d_data, cObject.id.replace('/', '∕'), self.d_collection)
-        self.__write_json__(filename, cObject)
-        return cObject
+    @abc.abstractmethod
+    def set_service(self, s_obj):
+        pass
 
-    def setMember(self, cid, mObject):
-        filename = join(self.d_data, cid.replace('/', '∕'), mObject.id.replace('/', '∕')+'.json')
-        self.__write_json__(filename, mObject)
-        return mObject
-
-    def delCollection(self, cid):
-        filename = join(self.d_data, cid.replace('/', '∕'))
-        rmtree(filename)
-        return True
-
-    def delMember(self, cid, mid):
-        filename = join(self.d_data, cid.replace('/', '∕'), mid.replace('/', '∕')+'.json')
-        remove(filename)
-        return True
-
-    def mintID(self):
-        return ''.join(random.choice(string.ascii_letters) for _ in range(random.randint(10, 30)))
-
-db = DataBase()
+    @abc.abstractmethod
+    def get_id(self, type):
+        pass
