8fb927343e09d3e922136d8b64b82c3a7f12ba40
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-03-23 11:52:15 -0400

    LDP store with interface

---
+++ src/data/ldp_db.py
@@ -0,0 +1,33 @@
+from .db import DBInterface
+
+class LDPDataBase(DBInterface):
+
+    def __init__(self, root):
+        assert False
+
+    def get_collection(self, id:None):
+        assert False
+
+    def set_collection(self, c_obj):
+        assert False
+
+    def del_collection(self, id):
+        assert False
+
+    def get_member(self, cid, mid:None):
+        assert False
+
+    def set_member(self, cid, m_obj):
+        assert False
+
+    def del_member(self, cid, mid):
+        assert False
+
+    def get_service(self):
+        assert False
+        
+    def set_service(self, s_obj):
+        assert False
+    
+    def get_id(self, type):
+        assert False
