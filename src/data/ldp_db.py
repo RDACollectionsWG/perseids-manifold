d921a7e011a60553f3e19f241a34dd1fb2186d55
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-03-24 12:12:58 -0400

    Properties map for conversion

--- src/data/ldp_db.py
+++ src/data/ldp_db.py
@@ -6,6 +6,32 @@ from src.collections.models import *
 from src.members.models import *
 
 LDP = Namespace("http://www.w3.org/ns/ldp#")
+RDA = Namespace("http://rd-alliance.org/ns/collections#")
+
+properties = {
+    'CollectionObject': {
+        str(DCTERMS.identifier): ['id', str, Literal],
+        str(DCTERMS.description): ['description', str, Literal],
+        str(RDA.hasCapabilities): ['capabilities', lambda x: x, lambda x: URIRef("#capabilities")],
+        str(RDA.hasProperties): ['properties', lambda x: x, lambda x: URIRef("#properties")]
+    },
+    'CollectionCapabilities': {
+        str(RDA.isOrdered): ['isOrdered', bool, Literal],
+        str(RDA.appendsToEnd): ['appendsToEnd', bool, Literal],
+        str(RDA.maxLength): ['maxLength', int, Literal],
+        str(RDA.membershipIsMutable): ['membershipIsMutable', bool, Literal],
+        str(RDA.metadataIsMutable): ['metadataIsMutable', bool, Literal],
+        str(RDA.restrictedToType): ['restrictedToType', bool, Literal],
+        str(RDA.supportsRole): ['supportsRoles', bool, Literal],
+    },
+    'CollectionProperties': {
+        str(RDA.modelType): ['modelType', str, URIRef],
+        str(RDA.descriptionOntology): ['descriptionOntology', str, URIRef],
+        str(DCTERMS.license): ['license', str, URIRef],
+        str(DCTERMS.rightsHolder): ['owner', str, Literal],
+        str(RDA.hasAccessRestrictions): ['hasAccessRestrictions', bool, Literal]
+    }
+}
 
 class LDPDataBase(DBInterface):
 
