2fedc1dd6cebbe246190acda155b0ecc788cf161
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-03-29 10:34:44 -0400

    Route for Swagger doc

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from src.app import CollectionsAPI
-
+from flask import send_from_directory
 from src.collections.routes import routes as r_collections
 from src.members.routes import routes as r_members
 from src.service.routes import routes as r_service
@@ -11,8 +11,13 @@ app.json_encoder = RDAJSONEncoder
 app.json_decoder = RDAJSONDecoder
 activate(app)
 
+
 for (url, kwargs) in r_service + r_collections + r_members:
     app.add_url_rule(url, **kwargs)
+
+def index():
+    return send_from_directory("www","index.html")
+app.add_url_rule("/", methods=["GET"], view_func=index)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
