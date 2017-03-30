24947f7f652f2d8b71744505867db7811238b538
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-03-30 13:44:47 -0400

    Make Model.dict() recursive

--- src/utils/models.py
+++ src/utils/models.py
@@ -1,3 +1,9 @@
+def resolve(obj):
+    if isinstance(obj, Model):
+        return obj.dict()
+    else:
+        return obj
+
 class Model(object):
 
     @classmethod
@@ -8,4 +14,4 @@ class Model(object):
             return None
 
     def dict(self):
-        return {key: value for key, value in self.__dict__.items() if not key.startswith("__")}
+        return {key: resolve(value) for key, value in self.__dict__.items() if not key.startswith("__") and value is not None}
