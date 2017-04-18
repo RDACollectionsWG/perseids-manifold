2a2a297c2635b9aedbc28beb45e91f16234270f5
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-04-18 15:30:24 -0400

    Make Model.dict() recursive

--- src/utils/models.py
+++ src/utils/models.py
@@ -1,6 +1,10 @@
 def resolve(obj):
     if isinstance(obj, Model):
         return obj.dict()
+    elif isinstance(obj, list):
+        return [resolve(o) for o in obj]
+    elif isinstance(obj, dict):
+        return {k: resolve(v) for k,v in obj.items() if not k.startswith("__")}
     else:
         return obj
 
