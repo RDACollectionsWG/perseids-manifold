935ca8562e674cb772423938fe35bc0a710ff4d7
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-04-25 11:43:55 -0400

    Add Marmotta utils

---
+++ src/utils/marmotta.py
@@ -0,0 +1,9 @@
+from rdflib import Namespace
+from src.utils.struct import Struct
+
+class Marmotta:
+
+    def __init__(self, srv):
+        self.server = Namespace(srv) if srv.endswith("/") else Namespace(srv+"/")
+        self.ldp = lambda slug=None: self.server.ldp if slug is None else self.server["ldp"+slug[:-1]] if slug.startswith("/") and slug.endswith("/") else self.server["ldp"+slug] if slug.startswith("/") else self.server["ldp/"+slug[:-1]] if slug.endswith("/") else self.server["ldp/"+slug]
+        self.sparql = Struct(select=self.server["sparql/select"], update=self.server["sparql/update"])
