c8e35638293a320779d1dc11eba2b8fbe7ccd396
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-05-05 10:53:16 -0400

    Switch to LDPDatabase backend

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from src.data.filesystem_db import FilesystemDB
+from src.data.ldp_db import LDPDataBase
 
 class CollectionsAPI(Flask):
 
     def __init__(self, name):
         super(CollectionsAPI, self).__init__(name)
-        self.db = FilesystemDB()
+        self.db = LDPDataBase("http://marmotta:8080/marmotta")
