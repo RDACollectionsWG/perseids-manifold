e282a901a909b725242eb3689157b81cbc84802f
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-05-24 06:50:27 -0400

    Guard function for Permissions

--- src/utils/access/permissions.py
+++ src/utils/access/permissions.py
@@ -1,6 +1,8 @@
 class Permissions(dict):
 
     def __init__(self, r=False, w=False, x=False):
+        if not (isinstance(r,bool) and isinstance(w,bool) and isinstance(x,bool)):
+            raise TypeError
         self.r = r
         self.w = w
         self.x = x
