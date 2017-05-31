99aab52f58ce4477efb8ade53ec11f371a778411
Bridget Almas <balmas@gmail.com>
2017-05-31 19:03:22 +0000

    fixes #48

--- src/utils/data/ldp_db.py
+++ src/utils/data/ldp_db.py
@@ -26,17 +26,17 @@ class LDPDataBase(DBInterface):
     def get_collection(self, id=None):
         # todo: ASK and check if collection exists
         if id is not None:
-            result = JSONResult(requests.post(self.marmotta.sparql.select, data=self.sparql.collections.select(self.marmotta.ldp(encoder.encode(id))), headers={"Accept":"application/sparql-results+json"}).json())
+            result = JSONResult(requests.post(self.marmotta.sparql.select, data=self.sparql.collections.select(self.marmotta.ldp(encoder.encode(id))), headers={"Accept":"application/sparql-results+json", "Content-Type":"application/sparql-select"}).json())
             graph = self.sparql.result_to_dataset(result).graph(self.marmotta.ldp(encoder.encode(id)))
             contents = self.RDA.graph_to_collection(graph)
             if len(contents) is 0:
                 raise KeyError
         else:
-            response = requests.post(self.marmotta.sparql.select, data=self.sparql.collections.list(s=self.marmotta.ldp(),p=LDP.ns.contains), headers={"Accept":"application/sparql-results+json"})
+            response = requests.post(self.marmotta.sparql.select, data=self.sparql.collections.list(s=self.marmotta.ldp(),p=LDP.ns.contains), headers={"Accept":"application/sparql-results+json", "Content-Type":"application/sparql-select"})
             collections = [dct[Variable('o')] for dct in JSONResult(response.json()).bindings]
             contents = []
             for collection in collections:
-                result = JSONResult(requests.post(self.marmotta.sparql.select, data=self.sparql.collections.select(collection), headers={"Accept":"application/sparql-results+json"}).json())
+                result = JSONResult(requests.post(self.marmotta.sparql.select, data=self.sparql.collections.select(collection), headers={"Accept":"application/sparql-results+json","Content-Type":"application/sparql-select"}).json())
                 graph = self.sparql.result_to_dataset(result).graph(collection)
                 contents += self.RDA.graph_to_collection(graph)
         return contents
@@ -61,7 +61,7 @@ class LDPDataBase(DBInterface):
             raise KeyError
 
     def del_collection(self, id):
-        found = JSONResult(requests.post(self.marmotta.sparql.select, data=self.sparql.collections.ask(self.marmotta.ldp(encoder.encode(id))), headers={"Accept":"application/sparql-results+json"}).json()).askAnswer
+        found = JSONResult(requests.post(self.marmotta.sparql.select, data=self.sparql.collections.ask(self.marmotta.ldp(encoder.encode(id))), headers={"Accept":"application/sparql-results+json","Content-Type":"application/sparql-select"}).json()).askAnswer
         if found:
             delete =self.sparql.collections.delete(self.marmotta.ldp(encoder.encode(id)))
             response = requests.post(self.marmotta.sparql.update, data=delete)
@@ -78,18 +78,18 @@ class LDPDataBase(DBInterface):
         # todo: ASK and check if member exists
         if mid is not None:
             id = self.marmotta.ldp(encoder.encode(cid)+"/member/"+encoder.encode(mid))
-            response = requests.post(self.marmotta.sparql.select, data=self.sparql.members.select(id), headers={"Accept":"application/sparql-results+json"})
+            response = requests.post(self.marmotta.sparql.select, data=self.sparql.members.select(id), headers={"Accept":"application/sparql-results+json","Content-Type":"application/sparql-select"})
             ds =self.sparql.result_to_dataset(JSONResult(response.json()))
             contents = self.RDA.graph_to_member(ds.graph(id))
             if len(contents) is 0:
                 raise KeyError
         else:
             listed = self.sparql.collections.list(s=self.marmotta.ldp(encoder.encode(cid)+"/member"), p=LDP.ns.contains)
-            response = requests.post(self.marmotta.sparql.select, data=listed, headers={"Accept":"application/sparql-results+json"})
+            response = requests.post(self.marmotta.sparql.select, data=listed, headers={"Accept":"application/sparql-results+json","Content-Type":"application/sparql-select"})
             members = [dct[Variable('o')] for dct in JSONResult(response.json()).bindings]
             contents = []
             for member in members:
-                result = JSONResult(requests.post(self.marmotta.sparql.select, data=self.sparql.members.select(member), headers={"Accept":"application/sparql-results+json"}).json())
+                result = JSONResult(requests.post(self.marmotta.sparql.select, data=self.sparql.members.select(member), headers={"Accept":"application/sparql-results+json","Content-Type":"application/sparql-select"}).json())
                 graph =self.sparql.result_to_dataset(result).graph(member)
                 contents += self.RDA.graph_to_member(graph)
         return contents
@@ -97,7 +97,7 @@ class LDPDataBase(DBInterface):
     def set_member(self, cid, m_obj):
         c_id = self.marmotta.ldp(encoder.encode(cid))
         m_id = self.marmotta.ldp(encoder.encode(cid)+"/member/"+encoder.encode(m_obj.id))
-        found = JSONResult(requests.post(self.marmotta.sparql.select, data=self.sparql.collections.ask(c_id), headers={"Accept":"application/sparql-results+json"}).json()).askAnswer
+        found = JSONResult(requests.post(self.marmotta.sparql.select, data=self.sparql.collections.ask(c_id), headers={"Accept":"application/sparql-results+json","Content-Type":"application/sparql-select"}).json()).askAnswer
         if not found:
             raise KeyError
         ds = Dataset()
@@ -114,7 +114,7 @@ class LDPDataBase(DBInterface):
 
     def del_member(self, cid, mid):
         id = self.marmotta.ldp(encoder.encode(cid)+"/member/"+encoder.encode(mid))
-        found = JSONResult(requests.post(self.marmotta.sparql.select, data=self.sparql.members.ask(id), headers={"Accept":"application/sparql-results+json"}).json()).askAnswer
+        found = JSONResult(requests.post(self.marmotta.sparql.select, data=self.sparql.members.ask(id), headers={"Accept":"application/sparql-results+json","Content-Type":"application/sparql-select"}).json()).askAnswer
         if found:
             delete = self.sparql.collections.delete(id)
             response = requests.post(self.marmotta.sparql.update, data=delete)
@@ -129,9 +129,9 @@ class LDPDataBase(DBInterface):
 
     def get_service(self):
         id = self.marmotta.ldp("service")
-        found = JSONResult(requests.post(self.marmotta.sparql.select, data=self.sparql.service.ask(id), headers={"Accept":"application/sparql-results+json"}).json()).askAnswer
+        found = JSONResult(requests.post(self.marmotta.sparql.select, data=self.sparql.service.ask(id), headers={"Accept":"application/sparql-results+json", "Content-Type":"application/sparql-select"}).json()).askAnswer
         if found:
-            response = requests.post(self.marmotta.sparql.select, data=self.sparql.service.select(id), headers={"Accept":"application/sparql-results+json"})
+            response = requests.post(self.marmotta.sparql.select, data=self.sparql.service.select(id), headers={"Accept":"application/sparql-results+json", "Content-Type":"application/sparql-select"})
             ds =self.sparql.result_to_dataset(JSONResult(response.json()))
             s_obj =self.RDA.graph_to_service(ds.graph(id))
             return s_obj
