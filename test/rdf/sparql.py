f60bdeb940a5a2ce429bfb3b2a3512410d398694
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-07-06 15:00:14 -0400

    Use new SPARQLTools constructor

--- test/rdf/sparql.py
+++ test/rdf/sparql.py
@@ -1,8 +1,10 @@
+import os
 from unittest import TestCase
 from rdflib import Variable
 from rdflib.plugins.sparql.results.jsonresults import JSONResult
 
 from src.utils.rdf.sparql import SPARQLTools
+from src.utils.ids.marmotta import Marmotta
 from test.mock import RandomGenerator
 
 
@@ -10,7 +12,7 @@ class SPARQLTest(TestCase):
 
     def setUp(self):
         self.mock = RandomGenerator()
-        self.sparql = SPARQLTools()
+        self.sparql = SPARQLTools(Marmotta(os.environ.get('COLLECTIONS_API_TEST_DB')).sparql)
 
     def test_ldp_result_to_dataset(self):
         result = self.mock.collection_result()
