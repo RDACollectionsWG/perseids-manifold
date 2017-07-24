122a9a8193437ba036958271b4d3e8e8785396e1
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-07-24 15:13:07 -0400

    Fix DB reset

--- test/routes/collection_tests.py
+++ test/routes/collection_tests.py
@@ -25,7 +25,7 @@ class CollectionTest(TestCase):
         if not self.server:
             raise EnvironmentError
         self.app.db = LDPDataBase(self.server)
-        requests.post(self.app.db.marmotta.sparql.update, data=reset_marmotta)
+        requests.post(self.app.db.marmotta.sparql.update, data=reset_marmotta, headers={"Content-Type":"application/sparql-update; charset=utf-8"})
         #self.dir = TemporaryDirectory(dir='test/data')
         # self.dir = mkdtemp(dir='test/data')
         #self.app.db = FilesystemDB(self.dir.name)
@@ -33,7 +33,7 @@ class CollectionTest(TestCase):
 
     def tearDown(self):
         #self.dir.cleanup()
-        requests.post(self.app.db.marmotta.sparql.update, data=reset_marmotta)
+        requests.post(self.app.db.marmotta.sparql.update, data=reset_marmotta, headers={"Content-Type":"application/sparql-update; charset=utf-8"})
 
     def to_dict(self, c_obj):
         if isinstance(c_obj, CollectionObject):
