f0dfe62a5809a90b0f4d9ba0f37ddbdce80e5a52
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-07-31 15:57:26 -0400

    Dynamic Swagger JSON at /apidocs

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
 import os
 
-from flask import send_from_directory
+from flask import send_from_directory, request, jsonify, make_response
 from flask_cors import CORS
+from urllib.parse import urlparse
 
 from config import Config, FilesystemDBConfig, ComposeConfig
 from src.app import CollectionsAPI
 from src.collections.routes import routes as r_collections
 from src.members.routes import routes as r_members
 from src.service.routes import routes as r_service
+from src.utils.base.swagger import swagger
 from src.utils.base.errors import activate
 from src.utils.conversions.json import *
 
@@ -37,8 +39,29 @@ def index():
 app.add_url_rule("/", methods=["GET"], view_func=index)
 
 def apidocs():
-    return send_from_directory("www","swagger.json")
+    url = urlparse(request.url)
+    if ":" in url.netloc:
+        host, port = url.netloc.split(":")
+    else:
+        host = url.netloc
+        port = "80"
+    base_path = url.path.replace('/apidocs','') if url.path != "/apidocs" else "/"
+    schemes = [url.scheme]
+    other_scheme = "https" if url.scheme is "http" else "http"
+    try:
+        if request.get(other_scheme+"://"+url.netloc+url.path.replace('/apidocs','')+"/scheme").status_code is 200:
+            schemes += [other_scheme]
+    except:
+        pass
+    r = make_response(swagger.json(schemes, host, port, base_path))
+    r.mimetype = 'application/json'
+    return r
+    # return send_from_directory("www","swagger.json")
 app.add_url_rule("/apidocs", methods=["GET"], view_func=apidocs)
+
+def scheme():
+    return jsonify(""), 200
+app.add_url_rule("/scheme", methods=["GET"], view_func=scheme)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
